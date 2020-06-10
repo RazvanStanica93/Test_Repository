717fd6450d72bdc730412a44a282ff8cdced0bd8
RazvanStanica <rstanica@ptc.com>
2020-06-10 13:19:18 +0300

    First commit

---
+++ Project_test/Project_test/Test1/MyClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_test.Test1
+{
+    class MyClass
+    {
+        public string t1 = "test in Folder1";
+    }
+}
---
+++ Project_test/Project_test/Test2/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_test.Test2
+{
+    class Class1
+    {
+        public string t = "test in Folder 2";
+     
+    }
+}
